4d8fca0e0ccccdf39ab591abb42335fea085110a
SupaHotMoj0 <101607979+SupaHotMoj0@users.noreply.github.com>
2024-04-11 00:37:39 +0200

    Update config_flow.py

--- custom_components/tuya_ble/config_flow.py
+++ custom_components/tuya_ble/config_flow.py
@@ -8,8 +8,10 @@ import pycountry
 from typing import Any
 
 import voluptuous as vol
-from tuya_iot import AuthType, TuyaCloudOpenAPIEndpoint
-
+from tuya_iot import {
+    AuthType,
+    TuyaCloudOpenAPIEndpoint,
+}
 from homeassistant.config_entries import (
     ConfigEntry,
     ConfigFlow,
